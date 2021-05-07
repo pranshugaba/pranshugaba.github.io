4e80f157f1bc0a118de222ab5ba9822fedffa4ef
Pranshu Gaba <pranshugaba@gmail.com>
2021-05-07 17:17:53 +0530

    Add ReadingList

--- components/navigation/SectionsHeader.vue
+++ components/navigation/SectionsHeader.vue
@@ -1,14 +1,11 @@
 <template>
   <nav>
-    <ul class="-mt-12 mb-8 flex space-x-4 items-center justify-between">
+    <ul class="-mt-12 mb-8 flex space-x-4 items-center justify-around">
       <li>
         <NuxtLink to="/posts">Posts</NuxtLink>
       </li>
       <li>
         <NuxtLink to="/poems">Poems</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/stories">Stories</NuxtLink>
       </li>
       <li>
         <NuxtLink to="/puzzles">Puzzles</NuxtLink>
---
+++ pages/reading-list.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <PostTitle>My Reading List</PostTitle>
+    <PostSubtitle>Some of the books I have read</PostSubtitle>
+    <ul>
+      <li>The Hobbit, or There and Back Again</li>
+      <li>Shutter Island</li>
+    </ul>
+  </div>
+</template>
