a79faecb824ca1984ba913cacd5a3544cb2dd56b
pranshugaba <pranshugaba@users.noreply.github.com>
2021-05-10 18:32:27 +0000

    deploy: 0d1eaf939e6514a56fca5b4472a68e71c8e37177

--- _nuxt/static/1620671531/poems/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-website\u002F_nuxt\u002Fstatic\u002F1620671531",layout:"default",error:a,serverRendered:false,routePath:"\u002Fpoems",config:{_app:{basePath:"\u002Fnuxt-website\u002F",assetsPath:"\u002Fnuxt-website\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8c6725d0"}}}}(null));
