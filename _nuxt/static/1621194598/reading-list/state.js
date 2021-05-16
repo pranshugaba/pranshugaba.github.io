875a657a2e2ebf300590b33b3e08d3f9ec0e43c5
pranshugaba <pranshugaba@users.noreply.github.com>
2021-05-16 19:50:11 +0000

    deploy: ea7749302573c345ee996379d30b85bbf2084c7e

---
+++ _nuxt/static/1621194598/reading-list/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621194598",layout:"default",error:a,serverRendered:b,routePath:"\u002Freading-list",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"623452ce"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
