3f5cff822ddead6c4de1df8056502fc6e5a4bec4
pranshugaba <pranshugaba@users.noreply.github.com>
2021-05-17 15:22:02 +0000

    deploy: 4568f4f4b10d267f107b222d24c7429afa46eb4b

---
+++ _nuxt/435c41c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{251:function(e,t,n){"use strict";n.r(t);var r={name:"vImage",props:["src"]},c=n(11),component=Object(c.a)(r,(function(){var e=this,t=e.$createElement;return(e._self._c||t)("img",{staticClass:"h-auto object-cover rounded-lg shadow-md",attrs:{src:e.src}})}),[],!1,null,null,null);t.default=component.exports}}]);
